74ba25d03e596e11c19256a429433b44882b945e
jenna <ravennasw@gmail.com>
2023-01-25 21:01:26 +0700

    add code and tests

---
+++ src/main/java/ru/netology/radio/Radio.java
@@ -0,0 +1,62 @@
+package ru.netology.radio;
+
+public class Radio {
+    public int currentStation;
+    public int currentVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation < 0) {
+            return;
+        }
+        if (newCurrentStation > 9) {
+            return;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void next() {
+        if (currentStation != 9) {
+            currentStation++;
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public void prev() {
+        if (currentStation != 0) {
+            currentStation--;
+        } else {
+            currentStation = 9;
+        }
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 10) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+}
---
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -0,0 +1,83 @@
+package ru.netology.radio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class RadioTest {
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/setStation.csv")
+    public void setStation(int currentStation, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentStation(currentStation);
+
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/nextStation.csv")
+    public void switchNextStation(int currentStation, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentStation(currentStation);
+
+        radio.next();
+
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/prevStation.csv")
+    public void switchPrevStation(int currentStation, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentStation(currentStation);
+
+        radio.prev();
+
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/setVolume.csv")
+    public void setVolume(int currentVolume, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(currentVolume);
+
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/increaseVolume.csv")
+    public void toIncreaseVolume(int currentVolume, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(currentVolume);
+
+        radio.increaseVolume();
+
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/decreaseVolume.csv")
+    public void toDecreaseVolume(int currentVolume, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(currentVolume);
+
+        radio.decreaseVolume();
+
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
